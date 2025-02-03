a58f02cc047de492812e31f3ac156ce5f71568fb
Mori Yasuaki <mori.yasuaki18@gmail.com>
2025-02-03 20:33:00 +0900

    prog in ch12

---
+++ code/ch12/script/test_hello.py
@@ -0,0 +1,6 @@
+from subprocess import run
+
+def test_hello():
+    result = run(["python", "hello.py"], capture_output=True, text=True)
+    output = result.stdout
+    assert output == "Hello world!\n"
